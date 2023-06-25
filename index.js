be2f97ff5c229526eab7c5acc8e9bb98cf170862
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-06-25 10:38:23 -0700

    added functions to create logo based on answers and prompt questions

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./questions.js');
+const questions = require('./questions.js');
+const fileName = "./Logos/logo.svg";
+const setShape = require('./lib/setShape.js')
+
+// function to create new svg file using inquirer response and file system
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo'));
+}
+
+// prompts questions then creates logo using responses
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
